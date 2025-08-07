5ddd51c47ac0947745befc353d42a412ad1146fa
sabritss <s.ramiles@gmail.com>
2025-08-06 23:14:21 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+// Importa o arquivo CSS global para aplicar os estilos do projeto.
+import "./styles/globals.css";
+
 import { Navigation } from "./components/Navigation";
 import { HeroSection } from "./components/HeroSection";
 import { StorySection } from "./components/StorySection";
@@ -10,6 +13,8 @@ import { TestimonialsSection } from "./components/TestimonialsSection";
 import { FinalCallSection } from "./components/FinalCallSection";
 import { Footer } from "./components/Footer";
 
+// O componente principal da sua aplicação.
+// Ele renderiza todas as seções da sua página.
 export default function App() {
   return (
     <div className="min-h-screen">
