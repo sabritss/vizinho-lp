70bff5ad3afa90466cd02bf87ab52a5683222bef
sabritss <s.ramiles@gmail.com>
2025-08-06 22:43:03 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import '/styles/globals.css'; // Troque pelo seu CSS principal, se for diferente
+import '../styles/globals.css'; // Troque pelo seu CSS principal, se for diferente
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
